b3c05fa57719ded79de2a135fa86fddc71541e00
ArberZe <arberzeka01@gmail.com>
2023-10-10 09:06:52 +0200

    Repository implementation, persistence service registration, connection string were added

--- MealPath.OrderManagement.Persistence/MealPathDbContext.cs
+++ MealPath.OrderManagement.Persistence/MealPathDbContext.cs
@@ -1,6 +1,31 @@
-﻿namespace MealPath.OrderManagement.Persistence
+﻿using MealPath.OrderManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealPath.OrderManagement.Persistence
 {
-    internal class MealPathDbContext
+    public class MealPathDbContext : DbContext
     {
+        public MealPathDbContext(DbContextOptions<MealPathDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MealPathDbContext).Assembly);
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = 1,
+                Name = "Ushqim"
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = 2,
+                Name = "Pije"
+            });
+        }
     }
 }
