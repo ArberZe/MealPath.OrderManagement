15f8f54fe5e7b1c3bed28c0850e60606b43f4bf0
erionismajli <erionismajli50@gmail.com>
2023-11-24 00:18:36 +0100

    Filter linked with categories

--- mealpath.ordermanagement.app/src/components/FilterContainer.jsx
+++ mealpath.ordermanagement.app/src/components/FilterContainer.jsx
@@ -27,6 +27,12 @@ const FilterContainer = () => {
     loadCategories();
   }, [categoryStore]);
 
+
+  if (!categoryStore.categories || categoryStore.categories.length === 0) {
+    return null;
+  }
+
+
   return (
     <section className="w-full my-6" id="menu">
     <div className="w-full flex flex-col items-center justify-center">
