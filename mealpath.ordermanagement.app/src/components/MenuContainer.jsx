c87ee0daa995e248b3fc07a21de2992aee7a21bb
erionismajli <erionismajli50@gmail.com>
2023-11-28 00:18:42 +0100

    menuContainer - change foodContainer

--- mealpath.ordermanagement.app/src/components/MenuContainer.jsx
+++ mealpath.ordermanagement.app/src/components/MenuContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import FilterContainer from "./FilterContainer";
 import FoodContainer from "./FoodContainer";
-import FoodContainer1 from "./FoodContainer1";
 
 
 const MenuContainer = () => {
@@ -10,7 +9,7 @@ const MenuContainer = () => {
   return (
     <section>
     <FilterContainer/>
-    <FoodContainer1/>
+    <FoodContainer/>
     </section>
   )
 }
