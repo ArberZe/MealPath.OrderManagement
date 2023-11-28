d9858c6a988d5886e493f7a1989937a4703a5896
erionismajli <erionismajli50@gmail.com>
2023-11-28 15:57:43 +0100

    dbContex-Updated

--- MealPath.OrderManagement.Persistence/Migrations/20231128145449_addProducts1.Designer.cs
+++ MealPath.OrderManagement.Persistence/Migrations/20231128145449_addProducts1.Designer.cs
@@ -4,6 +4,7 @@ using MealPath.OrderManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MealPath.OrderManagement.Persistence.Migrations
 {
     [DbContext(typeof(MealPathDbContext))]
-    partial class MealPathDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231128145449_addProducts1")]
+    partial class addProducts1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
